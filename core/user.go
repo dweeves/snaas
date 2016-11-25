cc84ab116a11e2ace121f709d5086dccfa80151c
Alexander Simmerl <xla@tapglue.com>
2016-11-25 18:16:58 +0100

    Implement App creation

--- core/user.go
+++ core/user.go
@@ -152,7 +152,7 @@ func UserListByEmails(
 	}
 }
 
-// UserListByPlatformIDs returns all users for the given ids for the social
+// UserListByPlatformIDsFunc returns all users for the given ids for the social
 // platform.
 type UserListByPlatformIDsFunc func(
 	currentApp *app.App,
